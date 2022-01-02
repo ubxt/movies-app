dd8ef895837cad480b900bf41e876d6b84bb1db9
ubxt <ubtofficial@hotmail.com>
2022-01-02 16:57:08 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import { GlobalStyle } from './GlobalStyle';
 //Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+//Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </div>
+const App = () => (
+	<Router>
+    	<Header />
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/:movieId' element={<Movie />} />
+			<Route path='/*' element={<NotFound />} />
+		</Routes>
+		<GlobalStyle />
+	</Router>
   );
-}
+
 
 export default App;
